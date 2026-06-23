15fa28cdd423b2dedea7d7e4fe0461d185bb8c87
gargbhavya2265 <bhavyagarg2265@gmail.com>
2026-06-23 14:28:13 +0530

    fix: kafka integration, auth/job service updates, docker improvements

--- services/auth/src/app.ts
+++ services/auth/src/app.ts
@@ -1,43 +1,13 @@
-// import express from "express";
-// import authRoutes from "./routes/auth.js";
-// import { connectKafka } from "./producer.js";
-// import cors from "cors";
-
-// const app = express();
-// const PORT = process.env.PORT || 5000;
-
-// app.use(cors());
-// app.use(express.json());
-
-// // routes FIRST
-// app.use("/api/auth", authRoutes);
-
-// const start = async () => {
-//   try {
-//     await connectKafka();
-
-//     app.listen(PORT, () => {
-//       console.log(`Auth service running on ${PORT}`);
-//     });
-//   } catch (err) {
-//     console.error("Failed to start service:", err);
-//     process.exit(1);
-//   }
-// };
-
-// start();
-
-// export default app;
-
-
 import express from "express";
 import authRoutes from "./routes/auth.js";
+import { connectKafka } from "./producer.js";
 import cors from "cors";
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
+
+connectKafka();
 
 app.use("/api/auth", authRoutes);
 
