c66643f96df2e5c656e0f39b0fed10081ff2a63a
gargbhavya2265 <bhavyagarg2265@gmail.com>
2026-06-30 17:16:45 +0530

    Debug API config

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -6,4 +6,8 @@ export const API = {
   PAYMENT: process.env.NEXT_PUBLIC_PAYMENT_SERVICE!,
 };
 
-console.log("API CONFIG:", API);
+console.log("AUTH =", process.env.NEXT_PUBLIC_AUTH_SERVICE);
+console.log("USER =", process.env.NEXT_PUBLIC_USER_SERVICE);
+console.log("JOB =", process.env.NEXT_PUBLIC_JOB_SERVICE);
+console.log("UTILS =", process.env.NEXT_PUBLIC_UTILS_SERVICE);
+console.log("PAYMENT =", process.env.NEXT_PUBLIC_PAYMENT_SERVICE);
