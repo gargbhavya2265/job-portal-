c5f9188b108da5caebffb092202871292e8b3855
gargbhavya2265 <bhavyagarg2265@gmail.com>
2026-06-30 17:37:06 +0530

    Temporarily hardcode frontend API URLs for testing

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,19 +1,7 @@
-const AUTH = process.env.NEXT_PUBLIC_AUTH_SERVICE;
-const USER = process.env.NEXT_PUBLIC_USER_SERVICE;
-const JOB = process.env.NEXT_PUBLIC_JOB_SERVICE;
-const UTILS = process.env.NEXT_PUBLIC_UTILS_SERVICE;
-
-console.log("========== API DEBUG ==========");
-console.log("AUTH:", AUTH);
-console.log("USER:", USER);
-console.log("JOB:", JOB);
-console.log("UTILS:", UTILS);
-console.log("===============================");
-
 export const API = {
-  AUTH,
-  USER,
-  JOB,
-  UTILS,
-  PAYMENT: process.env.NEXT_PUBLIC_PAYMENT_SERVICE,
+  AUTH: "https://job-portal-auth-i5fu.onrender.com",
+  USER: "https://job-portal-userservice.onrender.com",
+  JOB: "https://job-portal-jobservice.onrender.com",
+  UTILS: "https://job-portal-utilsservice.onrender.com",
+  PAYMENT: "",
 };
